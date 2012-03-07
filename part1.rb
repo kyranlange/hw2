59c88f242e314f969ccac95b0b2ead14964c131e
Kyran Lange <kyran.lange@gmail.com>
2012-03-07 15:25:16 +1030

    Added part 1 c)

--- part1.rb
+++ part1.rb
@@ -34,3 +34,19 @@ end
 
 puts "foo".palindrome?
 puts "A man, a plan, a canal -- Panama".palindrome?
+
+# c)
+module Enumerable
+  def palindrome?
+    if self.kind_of? Array
+      return self == self.reverse
+    else 
+      return self.to_a.palindrome?
+    end
+  end
+end
+
+puts [1,2,3,2,1].palindrome?
+puts [1,2,3,2,1,3].palindrome?
+ha = {"a" => "b", "c" => "d"}
+puts ha.palindrome?
