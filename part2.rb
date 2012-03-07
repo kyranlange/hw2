1ecef0a3faa43c4adbcb4d225374a1a5cfedec00
Kyran Lange <kyran.lange@gmail.com>
2012-03-07 15:34:23 +1030

    part 2

--- part2.rb
+++ part2.rb
@@ -1,6 +1,17 @@
 class CartesianProduct
   include Enumerable
-  # your code here
+  
+  def initialize(a,b)
+    @a, @b = a, b
+  end 
+  
+  def each
+    @a.each do |ael|
+      @b.each do |bel|
+        yield [ael, bel]
+      end
+    end
+  end
 end
 
 c = CartesianProduct.new([:a,:b], [4,5])
@@ -14,3 +25,7 @@ c = CartesianProduct.new([:a,:b], [])
 c.each { |elt| puts elt.inspect }
 # (nothing printed since Cartesian product
 # of anything with an empty collection is empty)
+
+c = CartesianProduct.new([:a,:b,:c], [4,5])
+c.each { |elt| puts elt.inspect }
+#[[:a,4],[:a,5],[:b,4],[:b,5],[:c,4],[:c,5]]
